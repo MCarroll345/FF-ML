c6b61c3ae4ce6172007e1aaf2494246ede38b2aa
MCarroll345 <118350383+MCarroll345@users.noreply.github.com>
2026-02-10 11:41:43 +0000

    Starting code

---
+++ app/config.py
@@ -0,0 +1,10 @@
+from dotenv import dotenv_values
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+config = dotenv_values(".env")
+
+client = MongoClient("mongodb+srv://MCarroll123:eebee261202@fitfinder.uzlpzrs.mongodb.net/?retryWrites=true&w=majority&appName=FitFinder", server_api=ServerApi('1'))
+db = client.clothes
+
+
